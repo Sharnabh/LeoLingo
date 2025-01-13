7a0413eb2c603ca81e68760476b7828b4f8fe0d5
aditya10bhardwaj <adityabhardwaj101004@gmail.com>
2025-01-13 15:24:52 +0530

    Added Data Model files.

--- LeoLingo/LeoLingo/Data Models/DataModel.swift
+++ LeoLingo/LeoLingo/Data Models/DataModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct Record {
+    var attempts: Int
+    var accuracy: Double
+    var recording: [String]
+}
+
+struct Word {
+    var wordTitle: String
+    var wordImage: String
+    var record: [Record]
+    var isPracticed: Bool
+}
+
+struct Level {
+    var levelTitle: String
+    var levelDescription: String
+    var words: [Word]
+}
+
+struct Card {
+    var cardTitle: String
+    var words: [Word]
+}
+
+struct Badge {
+    var badgeTitle: String
+    var badgeImage: String
+    var isAchieved: Bool
+}
+
+struct JungleRUn {
+    var backgroundImage: [String]
+    var avatarImage: [String]
+    var noOfLives: Int
+    var coins: Int
+    var diamonds: Int
+}

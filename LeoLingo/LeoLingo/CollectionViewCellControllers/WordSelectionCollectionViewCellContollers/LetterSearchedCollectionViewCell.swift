dbe079139a1414e40e867cb18f81467ceb6c6445
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2025-01-15 00:44:24 +0530

    Added search words

--- LeoLingo/LeoLingo/CollectionViewCellControllers/WordSelectionCollectionViewCellContollers/LetterSearchedCollectionViewCell.swift
+++ LeoLingo/LeoLingo/CollectionViewCellControllers/WordSelectionCollectionViewCellContollers/LetterSearchedCollectionViewCell.swift
@@ -7,6 +7,26 @@
 
 import UIKit
 
+protocol LetterSearchedDelegate: AnyObject {
+    func didTapRemoveButton(at indexPath: IndexPath)
+}
+
 class LetterSearchedCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var searchedWordLabel: UILabel!
+    @IBOutlet var removeButton: UIButton!
+    
+    weak var delegate: LetterSearchedDelegate?
+    var indexPath: IndexPath?
+    
+    func configureCell(with word: String, at indexPath: IndexPath) {
+        searchedWordLabel.text =  word
+        self.indexPath = indexPath
+    }
+
+    @IBAction func removeButtonTapped(_ sender: UIButton) {
+        guard let indexPath = indexPath else { return }
+        
+        self.delegate?.didTapRemoveButton(at: indexPath)
+    }
 }

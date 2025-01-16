d784b5dee876239cef9548b856565b77d749cd49
TARUN KUMAR <tarunmariya320@gmail.com>
2025-01-16 14:57:41 +0530

    Vocal coach main page added

---
+++ LeoLingo/LeoLingo/CollectionViewCellControllers/VocalCoach/SoundCardCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SoundCardCollectionViewCell.swift
+//  LeoLingo
+//
+//  Created by Batch - 2 on 16/01/25.
+//
+
+import UIKit
+
+class SoundCardCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ LeoLingo/LeoLingo/Controllers/VocalCoach/VocalCoachViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VocalCoachViewController.swift
+//  LeoLingo
+//
+//  Created by Batch - 2 on 16/01/25.
+//
+
+import UIKit
+
+class VocalCoachViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

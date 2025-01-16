b52f7d7b6e2ee4ae87fa71cf03111934a1eca82b
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2025-01-16 23:44:29 +0530

    Added fun learning and parent mode

--- LeoLingo/LeoLingo/Controllers/FunLearningViewController.swift
+++ LeoLingo/LeoLingo/Controllers/FunLearningViewController.swift
@@ -11,6 +11,11 @@ class FunLearningViewController: UIViewController {
 
     @IBOutlet var headingLabel: UILabel!
     @IBOutlet var gamesCollectionView: UICollectionView!
+//    @IBOutlet var nextButton: UIButton!
+//    @IBOutlet var previousButton: UIButton!
+    @IBOutlet var parentModeButton: UIButton!
+    
+    var currentIndex = 1
     
     let gameImages: [String] = ["JungleAdventureLogo", "JungleRunLogo", "SingAlongLogo"]
     
@@ -23,21 +28,60 @@ class FunLearningViewController: UIViewController {
         
         gamesCollectionView.backgroundColor = .none
 
-        headingLabel.layer.cornerRadius = 10
+        headingLabel.layer.cornerRadius = 20
+        headingLabel.layer.masksToBounds = true
         
         gamesCollectionView.delegate = self
         gamesCollectionView.dataSource = self
     }
 
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        let nextIndex = min(currentIndex + 1, gamesCollectionView.numberOfItems(inSection: 0) - 1)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        gamesCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        currentIndex = nextIndex
+    }
+    @IBAction func previousButtonTapped(_ sender: UIButton) {
+        let previousIndex = max(currentIndex - 1, 0)
+        let indexPath = IndexPath(item: previousIndex, section: 0)
+        gamesCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        currentIndex = previousIndex
+    }
+
+//     private func updateButtonStates() {
+//        let visibleItems = gamesCollectionView.indexPathsForVisibleItems.sorted()
+//        if let currentIndex = visibleItems.first {
+//            previousButton.isEnabled = currentIndex.item > 0
+//            nextButton.isEnabled = currentIndex.item < gameImages.count - 1
+//        }
+//    }
 }
 
 extension FunLearningViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+//    func updateIndex() {
+//        let visibleRect = CGRect(origin: gamesCollectionView.contentOffset, size: gamesCollectionView.bounds.size)
+//        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+//                
+//        if let indexPath = gamesCollectionView.indexPathForItem(at: visiblePoint) {
+//            currentIndex = indexPath.item
+//            print(currentIndex)
+//        }
+//    }
+//    
+//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        updateIndex()
+//    }
+//    
+//    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+//        updateIndex()
+//    }
     
     func configureLayout() -> UICollectionViewLayout {
         // Create item size
         let itemSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(0.5),     // Reduced from 0.7 to 0.5
-            heightDimension: .fractionalHeight(0.9)
+            widthDimension: .fractionalWidth(1),     // Reduced from 0.7 to 0.5
+            heightDimension: .fractionalHeight(1)
         )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
@@ -47,20 +91,20 @@ extension FunLearningViewController: UICollectionViewDelegate, UICollectionViewD
             heightDimension: .fractionalHeight(0.7)
         )
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.contentInsets = NSDirectionalEdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0)
         
         // Configure section
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .groupPagingCentered
-        section.interGroupSpacing = -50                 // Negative spacing to bring items closer
         
-        // Add visual effects
-        section.visibleItemsInvalidationHandler = { items, offset, environment in
-            items.forEach { item in
-                let distanceFromCenter = abs((item.frame.midX - offset.x) - environment.container.contentSize.width / 2.0)
-                let minScale: CGFloat = 0.8
-                let maxScale: CGFloat = 1.1
-                let scale = max(maxScale - (distanceFromCenter / environment.container.contentSize.width), minScale)
+        section.visibleItemsInvalidationHandler = { (visibleItems, offset, environment) in
+            let centerX = offset.x + environment.container.contentSize.width / 2
+            
+            visibleItems.forEach { item in
+                let distanceFromCenter = abs(item.frame.midX - centerX)
+                let scale = max(1.2 - (distanceFromCenter / environment.container.contentSize.width), 0.85)
                 item.transform = CGAffineTransform(scaleX: scale, y: scale)
+                item.zIndex = Int(scale * 10)
             }
         }
         
---
+++ LeoLingo/LeoLingo/Controllers/ParentMode/WordReportViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WordReportViewController.swift
+//  LeoLingo
+//
+//  Created by Sharnabh on 16/01/25.
+//
+
+import UIKit
+
+class WordReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.hidesBackButton = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

808a7f133fabf4ec5d7d059453e571de6fd67794
Sachin Tarkar <154526072+SachinTarkar842@users.noreply.github.com>
2025-01-19 19:51:23 +0530

    WordsInCards added

--- LeoLingo/LeoLingo/Controllers/WordsInCard/WordsInCardViewController.swift
+++ LeoLingo/LeoLingo/Controllers/WordsInCard/WordsInCardViewController.swift
@@ -1,29 +1,47 @@
-//
-//  WordsInCardViewController.swift
-//  LeoLingo
-//
-//  Created by Galgotias on 18/01/25.
-//
-
 import UIKit
 
-class WordsInCardViewController: UIViewController {
-
+class WordsInCardViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let items = [
+        ("Rat", "rat_image"),
+        ("Carrot", "carrot_image"),
+        ("Arm", "arm_image"),
+        ("Deer", "deer_image"),
+        ("Rabbit", "rabbit_image"),
+        ("Shirt", "shirt_image"),
+        ("Horse", "horse_image"),
+        ("Earth", "earth_image")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // Register your custom cell
+        collectionView.register(UINib(nibName: "CardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CardCell")
+        
+        // Configure the layout
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+            layout.itemSize = CGSize(width: 120, height: 150)
+            layout.minimumLineSpacing = 10
+            layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        let item = items[indexPath.row]
+        cell.configure(name: item.0, imageName: item.1)
+        return cell
+    }
 }

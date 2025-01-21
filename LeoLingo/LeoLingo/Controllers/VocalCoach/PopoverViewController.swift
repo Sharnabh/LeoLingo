cde940dcd9b03d0fefffafc289a53621d346763c
TARUN KUMAR <tarunmariya320@gmail.com>
2025-01-22 00:29:12 +0530

    popOver screen is added

--- LeoLingo/LeoLingo/Controllers/VocalCoach/PopoverViewController.swift
+++ LeoLingo/LeoLingo/Controllers/VocalCoach/PopoverViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class PopoverViewController: UIViewController {
 
+    @IBOutlet var levelBadge: UIImageView!
+    @IBOutlet var congratsLabel: UILabel!
+    
+    var message: String?
+       var onProceed: (() -> Void)?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        congratsLabel.text = message
         // Do any additional setup after loading the view.
     }
-    
+    @IBAction func proceedButtonTapped(_ sender: UIButton) {
+            dismiss(animated: true) {
+                self.onProceed?()
+            }
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }

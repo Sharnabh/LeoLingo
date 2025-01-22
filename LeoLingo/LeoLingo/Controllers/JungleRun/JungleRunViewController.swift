56e24364a8e21fc69e4273d8b1f489067a141d6a
TARUN KUMAR <tarunmariya320@gmail.com>
2025-01-22 11:50:11 +0530

    Jungle Run screens update , collision detection enabled

--- LeoLingo/LeoLingo/Controllers/JungleRun/JungleRunViewController.swift
+++ LeoLingo/LeoLingo/Controllers/JungleRun/JungleRunViewController.swift
@@ -1,24 +1,24 @@
 import UIKit
 
 class JungleRunViewController: UIViewController {
-    
-    var backgroundImage1: UIImageView!
-    var backgroundImage2: UIImageView!
-    var lionImageView: UIImageView!
+    @IBOutlet var backgroundImage1: UIImageView!
+    @IBOutlet var backgroundImage2: UIImageView!
+    @IBOutlet var lionImageView: UIImageView!
     @IBOutlet var coinLabel: UILabel!
     @IBOutlet var diamondLabel: UILabel!
     
-   
+    @IBOutlet var tapGesture: UITapGestureRecognizer!
     var hearts: [UIImageView] = []
     var coins: [UIImageView] = []
     
-    var coinLevel: Int = 0
-    var diamondLevel: Int = 0
+    var coinValue: Int = 0
+    var diamondValue: Int = 0
     var remainingHearts: Int = 5
     
     var wordCoinTimer: Timer?
     var wordCoin: UIImageView?
-    
+    var gameTimer: CADisplayLink?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,67 +26,76 @@ class JungleRunViewController: UIViewController {
         setupLion()
         setupLabels()
         setupHearts()
-        startBackgroundAnimation()
         setupTapGesture()
-        spawnCoins()
+        startBackgroundAnimation()
+        startGameLoop()
     }
     
+    // MARK: - Background Setup
     func setupBackground() {
         // Set up the first background image
-        backgroundImage1 = UIImageView(frame: view.bounds)
-        backgroundImage1.image = UIImage(named: "JungleBackground1")
+        backgroundImage1 = UIImageView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        backgroundImage1.image = UIImage(named: "JungleRunBackground1")
+        backgroundImage1.contentMode = .scaleAspectFill
         view.addSubview(backgroundImage1)
         
         // Set up the second background image, positioned to the right of the first
-        backgroundImage2 = UIImageView(frame: view.bounds)
-        backgroundImage2.image = UIImage(named: "JungleBackground2")
-        backgroundImage2.frame.origin.x = view.bounds.width
+        backgroundImage2 = UIImageView(frame: CGRect(x: view.bounds.width, y: 0, width: view.bounds.width, height: view.bounds.height))
+        backgroundImage2.image = UIImage(named: "JungleRunBackground2")
+        backgroundImage2.contentMode = .scaleAspectFill
         view.addSubview(backgroundImage2)
-        
-        // Start the infinite scrolling animation
-        startBackgroundAnimation()
     }
+
 
     func startBackgroundAnimation() {
-        let screenWidth = view.bounds.width
-        let animationDuration: TimeInterval = 5.0 // Adjust for desired scrolling speed
-        
-        // Animate the backgrounds
-        UIView.animate(withDuration: animationDuration, delay: 0, options: [.curveLinear, .repeat], animations: {
-            // Move both backgrounds to the left
-            self.backgroundImage1.frame.origin.x -= screenWidth
-            self.backgroundImage2.frame.origin.x -= screenWidth
-        }, completion: nil)
-        
-        // Schedule a timer to reposition the images when they move out of view
-        Timer.scheduledTimer(withTimeInterval: animationDuration, repeats: true) { _ in
-            if self.backgroundImage1.frame.origin.x + screenWidth <= 0 {
-                self.backgroundImage1.frame.origin.x = self.backgroundImage2.frame.maxX
-            }
-            if self.backgroundImage2.frame.origin.x + screenWidth <= 0 {
-                self.backgroundImage2.frame.origin.x = self.backgroundImage1.frame.maxX
-            }
-        }
-    }
-
-    
+           // Animate the background for infinite scrolling
+           let animationDuration: TimeInterval = 10.0
+           UIView.animate(withDuration: animationDuration, delay: 0, options: [.repeat, .curveLinear], animations: {
+               self.backgroundImage1.frame.origin.x -= self.view.bounds.width
+               self.backgroundImage2.frame.origin.x -= self.view.bounds.width
+           }, completion: { _ in
+               // Reset positions for infinite scrolling
+               self.backgroundImage1.frame.origin.x = 0
+               self.backgroundImage2.frame.origin.x = self.view.bounds.width
+           })
+       }
+//    func startBackgroundAnimation() {
+//        let screenWidth = view.bounds.width
+//        let animationDuration: TimeInterval = 10 // Adjust for desired speed
+//        
+//        // Animate both background images
+//        UIView.animate(withDuration: animationDuration, delay: 0, options: [.curveLinear, .repeat], animations: {
+//            self.backgroundImage1.frame.origin.x -= screenWidth
+//            self.backgroundImage2.frame.origin.x -= screenWidth
+//        }, completion: nil)
+//        
+//        // Use a timer to reset positions when images move out of view
+//        Timer.scheduledTimer(withTimeInterval: animationDuration, repeats: true) { _ in
+//            if self.backgroundImage1.frame.maxX <= 0 {
+//                self.backgroundImage1.frame.origin.x = self.backgroundImage2.frame.maxX
+//            }
+//            if self.backgroundImage2.frame.maxX <= 0 {
+//                self.backgroundImage2.frame.origin.x = self.backgroundImage1.frame.maxX
+//            }
+//        }
+//    }
+//    
+    // MARK: - Lion Setup
     func setupLion() {
-        let lionSize = CGSize(width: 100, height: 100)
-        lionImageView = UIImageView(frame: CGRect(x: 100, y: view.bounds.height - 200, width: lionSize.width, height: lionSize.height))
-        lionImageView.image = UIImage(named: "JungleLion")
-        view.addSubview(lionImageView)
-    }
+            let lionSize = CGSize(width: 150, height: 150) // Updated lion size
+            lionImageView = UIImageView(frame: CGRect(x: 100, y: view.bounds.height - 250, width: lionSize.width, height: lionSize.height))
+            lionImageView.image = UIImage(named: "JungleLion")
+            lionImageView.contentMode = .scaleAspectFit
+            view.addSubview(lionImageView)
+        }
     
+    // MARK: - UI Setup
     func setupLabels() {
-        coinLabel = UILabel(frame: CGRect(x: 20, y: 50, width: 150, height: 30))
         coinLabel.text = "🪙 0"
         coinLabel.textColor = .white
-        view.addSubview(coinLabel)
         
-        diamondLabel = UILabel(frame: CGRect(x: 200, y: 50, width: 150, height: 30))
         diamondLabel.text = "💎 0"
         diamondLabel.textColor = .white
-        view.addSubview(diamondLabel)
     }
     
     func setupHearts() {
@@ -98,60 +107,57 @@ class JungleRunViewController: UIViewController {
         }
     }
     
-    
-    
-    func setupTapGesture() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-        view.addGestureRecognizer(tapGesture)
-    }
-    
-    @objc func handleTap() {
-        if let wordCoin = wordCoin {
-            // Handle word coin tap
-            wordCoinTimer?.invalidate()
-            wordCoin.removeFromSuperview()
-            self.wordCoin = nil
-            diamondLevel += 1
-            updateDiamondLevel()
+    // MARK: - Game Logic
+    func startGameLoop() {
+            gameTimer = CADisplayLink(target: self, selector: #selector(updateGame))
+            gameTimer?.add(to: .main, forMode: .default)
+            spawnCoins()
         }
-    }
-    
+        
+        @objc func updateGame() {
+            detectCollisions()
+        }
+        
     func spawnCoins() {
-        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
-            let isWordCoin = Bool.random()
-            let coinImageName = isWordCoin ? "wordCoin" : "valueCoin"
-            let coin = UIImageView(image: UIImage(named: coinImageName))
-            coin.frame = CGRect(x: self.view.bounds.width, y: self.view.bounds.height - 250, width: 50, height: 50)
-            self.view.addSubview(coin)
-            self.coins.append(coin)
-            
-            UIView.animate(withDuration: 5.0, delay: 0, options: .curveLinear, animations: {
-                coin.frame.origin.x = -50
-            }, completion: { _ in
-                if let index = self.coins.firstIndex(of: coin) {
-                    self.coins.remove(at: index)
-                }
-                coin.removeFromSuperview()
-            })
+            Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { _ in
+                let isWordCoin = Bool.random()
+                let coinImageName = isWordCoin ? "wordCoin" : "valueCoin"
+                let coin = UIImageView(image: UIImage(named: coinImageName))
+                let randomY = self.view.bounds.height - CGFloat.random(in: 300...400) // Random height for variety
+                coin.frame = CGRect(x: self.view.bounds.width, y: randomY, width: 100, height: 100) // Updated coin size
+                self.view.addSubview(coin)
+                self.coins.append(coin)
+                
+                UIView.animate(withDuration: 5.0, delay: 0, options: .curveLinear, animations: {
+                    coin.frame.origin.x = -50
+                }, completion: { _ in
+                    if let index = self.coins.firstIndex(of: coin) {
+                        self.coins.remove(at: index)
+                    }
+                    coin.removeFromSuperview()
+                })
+            }
         }
-    }
+
     
     func detectCollisions() {
-        for coin in coins {
-            if lionImageView.frame.intersects(coin.frame) {
-                if coin.image == UIImage(named: "valueCoin") {
-                    coinLevel += 100
-                    updateCoinLevel()
-                } else if coin.image == UIImage(named: "wordCoin") {
-                    handleWordCoin(coin)
-                }
-                coin.removeFromSuperview()
-                if let index = coins.firstIndex(of: coin) {
-                    coins.remove(at: index)
+            for coin in coins {
+                if let coinFrame = coin.layer.presentation()?.frame,
+                   let lionFrame = lionImageView.layer.presentation()?.frame,
+                   lionFrame.intersects(coinFrame) {
+                    if coin.image == UIImage(named: "valueCoin") {
+                        coinValue += 100
+                        updateCoinLabel()
+                    } else if coin.image == UIImage(named: "wordCoin") {
+                        handleWordCoin(coin)
+                    }
+                    coin.removeFromSuperview()
+                    if let index = coins.firstIndex(of: coin) {
+                        coins.remove(at: index)
+                    }
                 }
             }
         }
-    }
     
     func handleWordCoin(_ coin: UIImageView) {
         wordCoin = coin
@@ -163,18 +169,19 @@ class JungleRunViewController: UIViewController {
         }
     }
     
-    func updateCoinLevel() {
-        coinLabel.text = "🪙 \(coinLevel)"
+    func updateCoinLabel() {
+        
+        coinLabel.text = "🪙 \(String(describing: coinValue))"
     }
     
     func updateDiamondLevel() {
-        diamondLabel.text = "💎 \(diamondLevel)"
+        diamondLabel.text = "💎 \(String(describing: coinValue))"
     }
     
     func loseHeart() {
         if remainingHearts > 0 {
             remainingHearts -= 1
-            hearts[remainingHearts].removeFromSuperview()
+            hearts[remainingHearts].isHidden = true
         }
         if remainingHearts == 0 {
             gameOver()
@@ -182,12 +189,29 @@ class JungleRunViewController: UIViewController {
     }
     
     func gameOver() {
-        // Handle game over logic
         print("Game Over")
+        gameTimer?.invalidate()
+        wordCoinTimer?.invalidate()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        detectCollisions()
-    }
-}
+    // MARK: - Gesture Handling
+    func setupTapGesture() {
+           // Add tap gesture to detect screen taps
+           let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+           view.addGestureRecognizer(tapGesture)
+       }
+       
+       @objc func handleTap() {
+           // Animate the lion's jump
+           let jumpHeight: CGFloat = 150
+           let jumpDuration: TimeInterval = 0.5
+           
+           UIView.animate(withDuration: jumpDuration / 2, animations: {
+               self.lionImageView.frame.origin.y -= jumpHeight
+           }, completion: { _ in
+               UIView.animate(withDuration: jumpDuration / 2, animations: {
+                   self.lionImageView.frame.origin.y += jumpHeight
+               })
+           })
+       }
+   }
